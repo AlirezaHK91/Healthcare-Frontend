5f2171669bbd920ac53ed5f4572ae178f716bbbe
Aldren <Aldrenpeshaia@hotmail.com>
2024-01-09 15:07:30 +0100

    Working on routing for the review

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
 import PrivateRoutes from "./utils/PrivateRoutes";
 import { AuthProvider } from "./context/Authcontext";
+import ReviewPage from "./pages/ReviewPage";
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
               </Layout>
             }>
             <Route path="/" element={<HomePage />} />
+            <Route path="/review" element={<ReviewPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/ReviewPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ReviewPage() {
+  return (
+    <div className="pt-56 bg-red-500 w-screen h-screen">
+      ReviewPagedsadadadadaadadadad
+    </div>
+  );
+}

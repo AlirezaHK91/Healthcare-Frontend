9213e54d0bec8ecfc5fa0ed482a479a358f79882
Aldren <Aldrenpeshaia@hotmail.com>
2024-01-09 23:12:31 +0100

    i have fixed that you can send reviews to the backend now.

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -1,22 +1,58 @@
 import { FaStar } from "react-icons/fa";
 import successfull from "../assets/successfull.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export default function ReviewPage() {
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
+  const [comment, setComment] = useState("");
+  const [user, setUserId] = useState(""); // State for the user ID
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      setUserId(storedUser.id);
+    }
+  }, []);
+
+  const handleSubmit = async () => {
+    if (rating === null || comment.trim() === "") {
+      alert("Please provide a rating, comment");
+      return;
+    }
+
+    const payload = {
+      user: { id: user },
+      rating: rating,
+      comment: comment,
+    };
+
+    try {
+      const response = await axios.post(`${apiUrl}/api/auth/review`, payload, {
+        withCredentials: true,
+      });
+
+      console.log("Review submitted successfully:", response.data);
+    } catch (error) {
+      console.error("Error submitting review:", error.message);
+      alert("Error submitting review. Please try again later.");
+    }
+  };
 
   return (
     <div className="flex flex-col">
       <div className="mx-auto mt-5 mb-3">
-        <img src={successfull} className="w-40" />
+        <img src={successfull} className="w-40" alt="Success Icon" />
       </div>
-      <div className="mx-auto mt-5 mb-3 flex ">
+      <div className="mx-auto mt-5 mb-3 flex">
         {/* review */}
         {[...Array(5)].map((star, index) => {
           const currentRating = index + 1;
           return (
-            <label>
+            <label key={currentRating}>
               <input
                 type="radio"
                 name="rating"
@@ -42,13 +78,17 @@ export default function ReviewPage() {
         <textarea
           placeholder="Leave feedback comment"
           rows={5}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
           className="block border-2 border-[#575757] w-full p-1 rounded-lg mb-4  focus:outline-none focus:border-blue"
         />
       </div>
+
       <div className="mx-auto mt-5 mb-3 ">
         {/* submit */}
         <button
-          type="submit"
+          type="button"
+          onClick={handleSubmit}
           className="w-full text-center py-3 border-2 border-[#575757] rounded-lg bg-[#575757] text-black hover:bg-green-dark focus:outline-none my-1">
           Submit review
         </button>

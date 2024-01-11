be8298fef627e5d9822bc1bd83f57aaa7a9c962a
Aldren <Aldrenpeshaia@hotmail.com>
2024-01-11 21:37:44 +0100

    Have changed that you can only review 1 selected booking

--- src/pages/ReviewPage.jsx
+++ src/pages/ReviewPage.jsx
@@ -1,5 +1,5 @@
 import { FaStar } from "react-icons/fa";
-import successfull from "../assets/successfull.png";
+import feedback from "../assets/feed.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -10,6 +10,10 @@ export default function ReviewPage() {
   const [hover, setHover] = useState(null);
   const [comment, setComment] = useState("");
   const [user, setUserId] = useState(""); // State for the user ID
+  const [bookings, setBookings] = useState([]);
+  const [selectedBooking, setSelectedBooking] = useState(null);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -18,9 +22,34 @@ export default function ReviewPage() {
     }
   }, []);
 
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const response = await axios.get(
+          `${apiUrl}/api/auth/booking/getAll/${user}`,
+          {
+            withCredentials: true,
+          }
+        );
+        const bookingData = response.data;
+
+        const filteredBookings = bookingData.filter(
+          (booking) => booking.isDone && !booking.review
+        );
+        setBookings(filteredBookings);
+        console.log("Filtered Bookings:", filteredBookings);
+      } catch (error) {
+        console.error("Error fetching bookings:", error.message);
+      }
+    };
+    if (user) {
+      fetchBookings();
+    }
+  }, [user]);
+
   const handleSubmit = async () => {
-    if (rating === null || comment.trim() === "") {
-      alert("Please provide a rating, comment");
+    if (rating === null || comment.trim() === "" || selectedBooking === null) {
+      setErrorMessage("Please provide a rating, comment");
       return;
     }
 
@@ -28,26 +57,53 @@ export default function ReviewPage() {
       user: { id: user },
       rating: rating,
       comment: comment,
+      booking: { id: selectedBooking.id },
     };
 
     try {
       const response = await axios.post(`${apiUrl}/api/auth/review`, payload, {
         withCredentials: true,
       });
-
+      setSuccessMessage("Review submitted successfully");
       console.log("Review submitted successfully:", response.data);
     } catch (error) {
+      setErrorMessage("Error submitting review. Please try again later.");
       console.error("Error submitting review:", error.message);
-      alert("Error submitting review. Please try again later.");
     }
   };
 
   return (
     <div className="flex flex-col">
       <div className="mx-auto mt-5 mb-3">
-        <img src={successfull} className="w-40" alt="Success Icon" />
+        <img src={feedback} className="w-40 mt-20" alt="Success Icon" />
       </div>
-      <div className="mx-auto mt-5 mb-3 flex">
+
+      {/* Select booking */}
+      <div className="mx-auto mt-5 mb-3">
+        <select
+          value={selectedBooking ? selectedBooking.id : ""}
+          onChange={(e) => {
+            const selectedId = e.target.value;
+
+            const selected = bookings.find(
+              (booking) => booking.id.toString() === selectedId
+            );
+
+            setSelectedBooking(selected || null);
+          }}
+          className="block border-2 border-[#575757] w-full p-1 rounded-lg mb-4 focus:outline-none focus:border-blue">
+          <option value="" disabled>
+            Select a booking
+          </option>
+          {bookings.map((booking) => (
+            <option key={booking.id} value={booking.id}>
+              Date: {booking.schedule.date} -- Time: {booking.schedule.time}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="mx-auto mb-3 flex">
         {/* review */}
         {[...Array(5)].map((star, index) => {
           const currentRating = index + 1;
@@ -60,11 +116,12 @@ export default function ReviewPage() {
                 onChange={() => setRating(currentRating)}
                 required
               />
+
               <FaStar
                 className="star"
                 size={30}
                 color={
-                  currentRating <= (hover || rating) ? "ffc107" : "#e4e5e9"
+                  currentRating <= (hover || rating) ? "#ffc107" : "#d4e5e9"
                 }
                 onMouseEnter={() => setHover(currentRating)}
                 onMouseLeave={() => setHover(null)}
@@ -80,18 +137,22 @@ export default function ReviewPage() {
           rows={5}
           value={comment}
           onChange={(e) => setComment(e.target.value)}
-          className="block border-2 border-[#575757] w-full p-1 rounded-lg mb-4  focus:outline-none focus:border-blue"
+          className="block border-2 border-[#575757] w-80 p-1 rounded-lg mb-4  focus:outline-none focus:border-blue"
         />
       </div>
 
-      <div className="mx-auto mt-5 mb-3 ">
+      <div className="mx-auto mb-3 ">
         {/* submit */}
         <button
           type="button"
           onClick={handleSubmit}
-          className="w-full text-center py-3 border-2 border-[#575757] rounded-lg bg-[#575757] text-black hover:bg-green-dark focus:outline-none my-1">
+          className="w-full text-center py-2 px-2 border-2 border-[#575757] rounded-lg bg-[#575757] text-black hover:bg-green-dark focus:outline-none my-1">
           Submit review
         </button>
+        {errorMessage && <p className="text-red-500 text-lg">{error}</p>}
+        {successMessage && (
+          <p className="text-green-700 text-md">{successMessage}</p>
+        )}
       </div>
     </div>
   );

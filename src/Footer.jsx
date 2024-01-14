26fbfae6a5d707f2c2325729d3cf018436f2f293
AlirezaHK91 <kasra_tehran101@hotmail.com>
2024-01-14 14:13:02 +0100

    Added feedback icon in footer and navigate to feedback.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,12 +3,16 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import fback from "./assets/fback.png"
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
+       <Link href="/review">
+        <img className='w-13 -my-9 ml-2' src={fback} alt="Feedback" />
+      </Link>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="">
         Healthcare AB
       </Link>{' '}
       {new Date().getFullYear()}
@@ -23,7 +27,7 @@ export default function Footer({title, description}) {
     <Box component="footer" sx={{ bgcolor: '#A3B8CB', py: 6 , position: "sticky"}}>
       <Container maxWidth="lg">
         
-        <Copyright />
+        <Copyright/>
 
       </Container>
     </Box>
